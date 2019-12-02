a80a4a1d72d0517b62a4de3e24c74b14545ed151
twosee <twose@qq.com>
2019-12-02 11:42:25 +0800

    Add base example for PDOPool

---
+++ example/pdo/base.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+use Swoole\Connection\Db\PDOConfig;
+use Swoole\Connection\Db\PDOPool;
+use Swoole\Coroutine;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+const N = 10000;
+
+$s = microtime(true);
+Coroutine\run(function () {
+    $pool = new PDOPool((new PDOConfig)
+        ->withHost('127.0.0.1')
+        ->withPort(3306)
+        ->withDbName('test')
+        ->withCharset('utf8')
+        ->withUsername('root')
+        ->withPassword('root')
+    );
+    for ($n = N; $n--;) {
+        Coroutine::create(function () use ($pool) {
+            $pdo = $pool->get();
+            $statement = $pdo->prepare('SELECT ? + ?');
+            if (!$statement) {
+                throw new RuntimeException('Prepare failed');
+            }
+            $a = mt_rand(1, 100);
+            $b = mt_rand(1, 100);
+            $result = $statement->execute([$a, $b]);
+            if (!$result) {
+                throw new RuntimeException('Execute failed');
+            }
+            $result = $statement->fetchAll();
+            if ($a + $b !== (int)$result[0][0]) {
+                throw new RuntimeException('Bad result');
+            }
+            $pool->put($pdo);
+        });
+    }
+});
+$s = microtime(true) - $s;
+echo 'Use ' . $s . 's for ' . N . ' queries' . PHP_EOL;
