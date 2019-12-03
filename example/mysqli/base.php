aae6964c2a6dcadc7c9bae8d279be6d09aea3fb1
twosee <twose@qq.com>
2019-12-03 16:41:27 +0800

    Support Mysqli

---
+++ example/mysqli/base.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+use Swoole\Coroutine;
+use Swoole\Database\MysqliConfig;
+use Swoole\Database\MysqliPool;
+use Swoole\Runtime;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+const N = 1;
+
+Runtime::enableCoroutine();
+$s = microtime(true);
+Coroutine\run(function () {
+    $pool = new MysqliPool((new MysqliConfig)
+        ->withHost('127.0.0.1')
+        ->withPort(3306)
+        // ->withUnixSocket('/tmp/mysql.sock')
+        ->withDbName('test')
+        ->withCharset('utf8mb4')
+        ->withUsername('root')
+        ->withPassword('root')
+    );
+    for ($n = N; $n--;) {
+        Coroutine::create(function () use ($pool) {
+            $mysqli = $pool->get();
+            $statement = $mysqli->prepare('SELECT ? + ?');
+            if (!$statement) {
+                throw new RuntimeException('Prepare failed');
+            }
+            $a = mt_rand(1, 100);
+            $b = mt_rand(1, 100);
+            if (!$statement->bind_param('dd', $a, $b)) {
+                throw new RuntimeException('Bind param failed');
+            }
+            if (!$statement->execute()) {
+                throw new RuntimeException('Execute failed');
+            }
+            if (!$statement->bind_result($result)) {
+                throw new RuntimeException('Bind result failed');
+            }
+            if (!$statement->fetch()) {
+                throw new RuntimeException('Fetch failed');
+            }
+            if ($a + $b !== (int)$result) {
+                throw new RuntimeException('Bad result');
+            }
+            $pool->put($mysqli);
+        });
+    }
+});
+$s = microtime(true) - $s;
+echo 'Use ' . $s . 's for ' . N . ' queries' . PHP_EOL;
