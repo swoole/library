736feacf3b5f8621175b78b9c3e1747011baed97
twosee <twose@qq.com>
2020-02-03 15:08:02 +0800

    Use root

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -8,8 +8,8 @@ if (!defined('SWOOLE_LIBRARY')) {
 if (!defined('MYSQL_SERVER_HOST')) {
     define('MYSQL_SERVER_HOST', 'mysql');
     define('MYSQL_SERVER_PORT', 3306);
-    define('MYSQL_SERVER_USER', 'username');
-    define('MYSQL_SERVER_PWD', 'password');
+    define('MYSQL_SERVER_USER', 'root');
+    define('MYSQL_SERVER_PWD', 'root');
     define('MYSQL_SERVER_DB', 'test');
 }
 
