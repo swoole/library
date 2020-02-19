48c0b5a91d22f4f0ff54ebf84861b29932cdd36f
twosee <twose@qq.com>
2020-02-19 14:03:53 +0800

    PSR-12: Missing visibility definition

--- src/core/FastCGI.php
+++ src/core/FastCGI.php
@@ -20,75 +20,75 @@ class FastCGI
      * Number of bytes in a FCGI_Header.  Future versions of the protocol
      * will not reduce this number.
      */
-    const HEADER_LEN = 8;
+    public const HEADER_LEN = 8;
 
     /**
      * Format of FCGI_HEADER for unpacking in PHP
      */
-    const HEADER_FORMAT = 'Cversion/Ctype/nrequestId/ncontentLength/CpaddingLength/Creserved';
+    public const HEADER_FORMAT = 'Cversion/Ctype/nrequestId/ncontentLength/CpaddingLength/Creserved';
 
     /**
      * Max content length of a record
      */
-    const MAX_CONTENT_LENGTH = 65535;
+    public const MAX_CONTENT_LENGTH = 65535;
 
     /**
      * Value for version component of FCGI_Header
      */
-    const VERSION_1 = 1;
+    public const VERSION_1 = 1;
 
     /**
      * Values for type component of FCGI_Header
      */
-    const BEGIN_REQUEST = 1;
+    public const BEGIN_REQUEST = 1;
 
-    const ABORT_REQUEST = 2;
+    public const ABORT_REQUEST = 2;
 
-    const END_REQUEST = 3;
+    public const END_REQUEST = 3;
 
-    const PARAMS = 4;
+    public const PARAMS = 4;
 
-    const STDIN = 5;
+    public const STDIN = 5;
 
-    const STDOUT = 6;
+    public const STDOUT = 6;
 
-    const STDERR = 7;
+    public const STDERR = 7;
 
-    const DATA = 8;
+    public const DATA = 8;
 
-    const GET_VALUES = 9;
+    public const GET_VALUES = 9;
 
-    const GET_VALUES_RESULT = 10;
+    public const GET_VALUES_RESULT = 10;
 
-    const UNKNOWN_TYPE = 11;
+    public const UNKNOWN_TYPE = 11;
 
     /**
      * Value for requestId component of FCGI_Header
      */
-    const DEFAULT_REQUEST_ID = 1;
+    public const DEFAULT_REQUEST_ID = 1;
 
     /**
      * Mask for flags component of FCGI_BeginRequestBody
      */
-    const KEEP_CONN = 1;
+    public const KEEP_CONN = 1;
 
     /**
      * Values for role component of FCGI_BeginRequestBody
      */
-    const RESPONDER = 1;
+    public const RESPONDER = 1;
 
-    const AUTHORIZER = 2;
+    public const AUTHORIZER = 2;
 
-    const FILTER = 3;
+    public const FILTER = 3;
 
     /**
      * Values for protocolStatus component of FCGI_EndRequestBody
      */
-    const REQUEST_COMPLETE = 0;
+    public const REQUEST_COMPLETE = 0;
 
-    const CANT_MPX_CONN = 1;
+    public const CANT_MPX_CONN = 1;
 
-    const OVERLOADED = 2;
+    public const OVERLOADED = 2;
 
-    const UNKNOWN_ROLE = 3;
+    public const UNKNOWN_ROLE = 3;
 }
