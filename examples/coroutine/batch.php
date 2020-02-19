f29e67f244822022d1db564695394289693a7184
twosee <twose@qq.com>
2020-02-19 15:44:55 +0800

    Add Coroutine\batch function

---
+++ examples/coroutine/batch.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of Swoole.
+ *
+ * @link     https://www.swoole.com
+ * @contact  team@swoole.com
+ * @license  https://github.com/swoole/library/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+use Swoole\Coroutine;
+use function Swoole\Coroutine\batch;
+
+require __DIR__ . '/../bootstrap.php';
+
+Coroutine::set(['hook_flags' => SWOOLE_HOOK_ALL]);
+
+Coroutine\run(function () {
+    $use = microtime(true);
+    $results = batch([
+        'gethostbyname' => function () {
+            return gethostbyname('localhost');
+        },
+        'file_get_contents' => function () {
+            return file_get_contents(__DIR__ . '/greeter.txt');
+        },
+        'sleep' => function () {
+            sleep(1);
+            return true;
+        },
+        'usleep' => function () {
+            usleep(1000);
+            return true;
+        },
+    ], 0.1);
+    $use = microtime(true) - $use;
+    echo "Use {$use}s, Result:\n";
+    var_dump($results);
+});
+echo "Done\n";
