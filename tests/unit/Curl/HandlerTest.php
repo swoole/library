fabf05d503b31d6f9611a5edd6f9af00d0a81cae
Demin Yin <deminy@deminy.net>
2020-04-12 13:07:40 -0700

    check if curl hook is enabled properly or not in unit test

--- tests/unit/Curl/HandlerTest.php
+++ tests/unit/Curl/HandlerTest.php
@@ -23,23 +23,22 @@ use Swoole\Runtime;
  */
 class HandlerTest extends TestCase
 {
+    /**
+     * @covers \Swoole\Curl\Handler::execute()
+     */
     public function testRedirect()
     {
         Runtime::enableCoroutine(SWOOLE_HOOK_CURL);
         Coroutine\run(function () {
-            $url = 'https://shorturl.at/wxWX4';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
+            $ch = curl_init('https://shorturl.at/wxWX4');
+            self::assertInstanceOf(Handler::class, $ch, 'Variable $ch should be a Handler object instead of a curl resource');
+
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
             curl_exec($ch);
             $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
-            self::assertEquals(200, $httpCode);
+            self::assertEquals(200, $httpCode, 'HTTP status code should be 200 instead of 301');
         });
     }
 }
