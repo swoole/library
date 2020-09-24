ebf5cb61bf3963f5f6f491423e3563a67f305817
matyhtf <mikan.tenny@gmail.com>
2020-09-24 16:31:07 +0800

    fix unavailable without swoole

---
+++ src/core/Exception.php
@@ -0,0 +1,6 @@
+<?php
+namespace Swoole;
+
+class Exception extends \RuntimeException {
+
+}
--- src/functions.php
+++ src/functions.php
@@ -9,6 +9,10 @@
 
 declare(strict_types=1);
 
+if (!extension_loaded('swoole')) {
+    define('SWOOLE_USE_SHORTNAME', true);
+}
+
 if (SWOOLE_USE_SHORTNAME) {
     function _string(string $string = ''): Swoole\StringObject
     {
