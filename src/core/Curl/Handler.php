swoole/library

commit 0f955ab8ad1ecd707f3cc873b8cc07d4fb11c3ef

Handle cURL header names in case insensitive manner (#76)
* Make cURL headers case-insensitive

As per RFC 2616 Section 4.2 HTTP headers should be case-insensitive. Some of the internal functions, such as `curl_setopt()` modify headers using their canonical name, such as `Content-Type`, `Cookie`, `Referer`, etc. However, using a canonical name might not always work properly, if the client has set a header name in the lower case, such as `content-type`, `referer` or upper case `CONTENT-TYPE`, `COOKIE` or any other mixture of letter casing. In order to fix this problem a new mapping beetwen the lower case header name representation and the actual header name is introduced and is now used when performing any header manipulations.

* Remove redundant code

* Remove empty header by setHeader()

* Fix missing headers

Co-authored-by: twosee <twose@qq.com>
Co-authored-by: sy-records <52o@qq52o.cn>

src/core/Curl/Handler.php
@@ -115,6 +115,8 @@ final class Handler
 
     private $headers = [];
 
+    private $headerMap = [];
+
     private $transfer;
 
     private $errCode = 0;
@@ -310,6 +312,28 @@ final class Handler
         $this->errMsg = $msg ? $msg : curl_strerror($code);
     }
 
+    private function hasHeader(string $headerName): bool
+    {
+        return isset($this->headerMap[strtolower($headerName)]);
+    }
+
+    private function setHeader(string $headerName, string $value): void
+    {
+        $lowerCaseHeaderName = strtolower($headerName);
+
+        if (isset($this->headerMap[$lowerCaseHeaderName])) {
+            unset($this->headers[$this->headerMap[$lowerCaseHeaderName]]);
+        }
+
+        if ($value !== '') {
+            $this->headers[$headerName] = $value;
+            $this->headerMap[$lowerCaseHeaderName] = $headerName;
+        } else {
+            // remove empty headers (keep same with raw cURL)
+            unset($this->headerMap[$lowerCaseHeaderName]);
+        }
+    }
+
     /**
      * @param mixed $value
      * @throws Swoole\Curl\Exception
@@ -360,7 +384,7 @@ final class Handler
                         break;
                     }
                 }
-                $this->headers['Accept-Encoding'] = $value;
+                $this->setHeader('Accept-Encoding', $value);
                 break;
             case CURLOPT_PROXYTYPE:
                 if ($value !== CURLPROXY_HTTP and $value !== CURLPROXY_SOCKS5) {
@@ -477,14 +501,11 @@ final class Handler
                     $header = explode(':', $header, 2);
                     $headerName = $header[0];
                     $headerValue = trim($header[1] ?? '');
-                    if (strlen($headerValue) === 0) {
-                        continue;
-                    }
-                    $this->headers[$headerName] = $headerValue;
+                    $this->setHeader($headerName, $headerValue);
                 }
                 break;
             case CURLOPT_REFERER:
-                $this->headers['Referer'] = $value;
+                $this->setHeader('Referer', $value);
                 break;
             case CURLINFO_HEADER_OUT:
                 $this->withHeaderOut = boolval($value);
@@ -493,7 +514,7 @@ final class Handler
                 $this->withFileTime = boolval($value);
                 break;
             case CURLOPT_USERAGENT:
-                $this->headers['User-Agent'] = $value;
+                $this->setHeader('User-Agent', $value);
                 break;
             case CURLOPT_CUSTOMREQUEST:
                 $this->method = (string) $value;
@@ -521,7 +542,7 @@ final class Handler
              * Http Cookie
              */
             case CURLOPT_COOKIE:
-                $this->headers['Cookie'] = $value;
+                $this->setHeader('Cookie', $value);
                 break;
             case CURLOPT_CONNECTTIMEOUT:
                 $this->clientOptions[Constant::OPTION_CONNECT_TIMEOUT] = $value;
@@ -560,7 +581,7 @@ final class Handler
                 }
                 break;
             case CURLOPT_USERPWD:
-                $this->headers['Authorization'] = 'Basic ' . base64_encode($value);
+                $this->setHeader('Cookie', 'Basic ' . base64_encode($value));
                 break;
             case CURLOPT_FOLLOWLOCATION:
                 $this->followLocation = $value;
@@ -683,8 +704,8 @@ final class Handler
                 // POST data
                 if ($this->postData) {
                     if (is_string($this->postData)) {
-                        if (empty($this->headers['Content-Type'])) {
-                            $this->headers['Content-Type'] = 'application/x-www-form-urlencoded';
+                        if (!$this->hasHeader('content-type')) {
+                            $this->setHeader('Content-Type', 'application/x-www-form-urlencoded');
                         }
                     } elseif (is_array($this->postData)) {
                         foreach ($this->postData as $k => $v) {
@@ -703,12 +724,6 @@ final class Handler
             // Notice: setHeaders must be placed last, because headers may be changed by other parts
             // As much as possible to ensure that Host is the first header.
             // See: http://tools.ietf.org/html/rfc7230#section-5.4
-            foreach ($this->headers as $headerName => $headerValue) {
-                if ($headerValue === '') {
-                    // remove empty headers (keep same with raw cURL)
-                    unset($this->headers[$headerName]);
-                }
-            }
             $client->setHeaders($this->headers);
             /**
              * Execute.
@@ -737,7 +752,7 @@ final class Handler
                         $this->method = 'GET';
                     }
                     if ($this->autoReferer) {
-                        $this->headers['Referer'] = $this->info['url'];
+                        $this->setHeader('Referer', $this->info['url']);
                     }
                     $this->setUrl($redirectUrl, false);
                     $this->setUrlInfo($redirectParsedUrl);

commit 7952a7b69374058b888e279d996b8b8a4e286b44

Fix typo
Introduced in #76

src/core/Curl/Handler.php
@@ -600,7 +600,7 @@ final class Handler
                 }
                 break;
             case CURLOPT_USERPWD:
-                $this->setHeader('Cookie', 'Basic ' . base64_encode($value));
+                $this->setHeader('Authorization', 'Basic ' . base64_encode($value));
                 break;
             case CURLOPT_FOLLOWLOCATION:
                 $this->followLocation = $value;