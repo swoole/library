4d8cd8ef61c096dcc8807df8d9d540c936755097
matyhtf <mikan.tenny@gmail.com>
2021-02-22 16:09:23 +0800

    fix tests [2]

--- tests/unit/Coroutine/HttpFunctionTest.php
+++ tests/unit/Coroutine/HttpFunctionTest.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 namespace Swoole\Coroutine;
 
 use PHPUnit\Framework\TestCase;
-use Swoole\Coroutine;
-
 use function Swoole\Coroutine\Http\get;
 use function Swoole\Coroutine\Http\post;
 
@@ -26,7 +24,7 @@ class HttpFunctionTest extends TestCase
     public function testGet()
     {
         run(function () {
-            $data = get("http://httpbin.org/get?hello=world");
+            $data = get('http://httpbin.org/get?hello=world');
             $body = json_decode($data->getBody());
             self::assertSame($body->headers->Host, 'httpbin.org');
             self::assertSame($body->args->hello, 'world');
@@ -37,7 +35,7 @@ class HttpFunctionTest extends TestCase
     {
         run(function () {
             $random_data = base64_encode(random_bytes(128));
-            $data = post("http://httpbin.org/post?hello=world", ['random_data' => $random_data]);
+            $data = post('http://httpbin.org/post?hello=world', ['random_data' => $random_data]);
             $body = json_decode($data->getBody());
             self::assertSame($body->headers->Host, 'httpbin.org');
             self::assertSame($body->args->hello, 'world');
