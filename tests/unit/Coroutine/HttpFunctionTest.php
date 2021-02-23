b6b1e231073703babeaab4ce86476c97f6d7380c
Demin Yin <deminy@deminy.net>
2021-02-22 21:11:25 -0800

    [#99] improvements on unit tests

--- tests/unit/Coroutine/HttpFunctionTest.php
+++ tests/unit/Coroutine/HttpFunctionTest.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace Swoole\Coroutine;
 
 use PHPUnit\Framework\TestCase;
+use Swoole\Coroutine;
 use function Swoole\Coroutine\Http\get;
 use function Swoole\Coroutine\Http\post;
 
@@ -24,10 +25,16 @@ class HttpFunctionTest extends TestCase
     public function testGet()
     {
         run(function () {
-            $data = get('http://httpbin.org/get?hello=world');
-            $body = json_decode($data->getBody());
-            self::assertSame($body->headers->Host, 'httpbin.org');
-            self::assertSame($body->args->hello, 'world');
+            Coroutine::create(function () {
+                self::assertSame(200, get('http://httpbin.org')->getStatusCode(), 'Test HTTP GET without query strings.');
+            });
+
+            Coroutine::create(function () {
+                $data = get('http://httpbin.org/get?hello=world');
+                $body = json_decode($data->getBody());
+                self::assertSame('httpbin.org', $body->headers->Host);
+                self::assertSame('world', $body->args->hello);
+            });
         });
     }
 
@@ -37,9 +44,9 @@ class HttpFunctionTest extends TestCase
             $random_data = base64_encode(random_bytes(128));
             $data = post('http://httpbin.org/post?hello=world', ['random_data' => $random_data]);
             $body = json_decode($data->getBody());
-            self::assertSame($body->headers->Host, 'httpbin.org');
-            self::assertSame($body->args->hello, 'world');
-            self::assertSame($body->form->random_data, $random_data);
+            self::assertSame('httpbin.org', $body->headers->Host);
+            self::assertSame('world', $body->args->hello);
+            self::assertSame($random_data, $body->form->random_data);
         });
     }
 }
