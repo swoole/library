4063926a0510f99f653dd71b68501a09508837be
sy-records <52o@qq52o.cn>
2021-06-24 10:06:23 +0800

    cs-fix

--- src/core/Database/PDOStatementProxy.php
+++ src/core/Database/PDOStatementProxy.php
@@ -139,29 +139,27 @@ class PDOStatementProxy extends ObjectProxy
     {
         if ($mode == PDO::FETCH_COLUMN) {
             if (empty($args)) {
-                throw new \Exception("2nd parameter \"colno\" is missing");
+                throw new \Exception('2nd parameter "colno" is missing');
             }
-            list($colno) = $args;
-            $this->setFetchModeContext = [$mode, (int)$colno];
-            return $this->__object->setFetchMode($mode, (int)$colno);
+            [$colno] = $args;
+            $this->setFetchModeContext = [$mode, (int) $colno];
+            return $this->__object->setFetchMode($mode, (int) $colno);
         }
 
         if ($mode == PDO::FETCH_CLASS) {
             if (count($args) >= 2) {
-                list($class, $constructorArgs) = $args;
+                [$class, $constructorArgs] = $args;
                 if (!is_null($constructorArgs) && !is_array($constructorArgs)) {
-                    throw new \Exception("3rd parameter \"constructArgs\" must be NULL or Array");
+                    throw new \Exception('3rd parameter "constructArgs" must be NULL or Array');
                 }
-            }
-            elseif (count($args) == 1) {
-                list($class) = $args;
-                $constructorArgs = NULL; // NULL|array
-            }
-            else {
-                throw new \Exception("2nd parameter \"class\" is missing");
+            } elseif (count($args) == 1) {
+                [$class] = $args;
+                $constructorArgs = null; // NULL|array
+            } else {
+                throw new \Exception('2nd parameter "class" is missing');
             }
             if (empty($class) || !class_exists($class)) {
-                throw new \Exception("2nd parameter must be valid class for setFetchMode(FETCH_CLASS, class, constructorArgs)");
+                throw new \Exception('2nd parameter must be valid class for setFetchMode(FETCH_CLASS, class, constructorArgs)');
             }
             $this->setFetchModeContext = [$mode, $class, $constructorArgs];
             return $this->__object->setFetchMode($mode, $class, $constructorArgs);
@@ -169,11 +167,11 @@ class PDOStatementProxy extends ObjectProxy
 
         if ($mode == PDO::FETCH_INTO) {
             if (empty($args)) {
-                throw new \Exception("2nd parameter \"object\" is missing");
+                throw new \Exception('2nd parameter "object" is missing');
             }
-            list($object) = $args;
-            if (! is_object($object)) {
-                throw new \Exception("2nd parameter must be object for setFetchMode(FETCH_INTO, object)");
+            [$object] = $args;
+            if (!is_object($object)) {
+                throw new \Exception('2nd parameter must be object for setFetchMode(FETCH_INTO, object)');
             }
             $this->setFetchModeContext = [$mode, $object];
             return $this->__object->setFetchMode($mode, $object);
