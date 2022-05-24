732621c29a362cb164aabb7d41863a9f6b14ebce
沈唁 <52o@qq52o.cn>
2022-05-24 13:06:32 +0800

    Fix style check error (#150)

--- src/core/Server/Helper.php
+++ src/core/Server/Helper.php
@@ -196,7 +196,7 @@ class Helper
 
         foreach ($input_options as $k => $v) {
             if (!array_key_exists(strtolower($k), $const_options)) {
-                //TODO throw exception
+                // TODO throw exception
                 trigger_error("unsupported option [{$k}]", E_USER_WARNING);
                 debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
             }
