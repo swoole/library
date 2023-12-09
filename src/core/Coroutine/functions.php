783b0d50f8f058f0d3bd3f8f002c50312121d6d3
Demin Yin <deminy@deminy.net>
2023-12-09 14:03:57 -0800

    update comments and changelogs

--- src/core/Coroutine/functions.php
+++ src/core/Coroutine/functions.php
@@ -61,6 +61,20 @@ function parallel(int $n, callable $fn): void
     $wg->wait();
 }
 
+/**
+ * Applies the callback to the elements of the given list.
+ *
+ * The callback function takes on two parameters. The list parameter's value being the first, and the key/index second.
+ * Each callback runs in a new coroutine, allowing the list to be processed in parallel.
+ *
+ * @param array $list A list of key/value paired input data.
+ * @param callable $fn The callback function to apply to each item on the list. The callback takes on two parameters.
+ *                     The list parameter's value being the first, and the key/index second.
+ * @param float $timeout > 0 means waiting for the specified number of seconds. other means no waiting.
+ * @return array Returns an array containing the results of applying the callback function to the corresponding value
+ *               and key of the list (used as arguments for the callback). The returned array will preserve the keys of
+ *               the list.
+ */
 function map(array $list, callable $fn, float $timeout = -1): array
 {
     $wg = new WaitGroup(count($list));
