cef8e6dfff0f9c4a308c5a1ab995c0a82584fceb
Demin Yin <deminy@deminy.net>
2024-02-25 22:52:57 -0800

    add unit tests and update CHANGELOG for MR #169

---
+++ tests/unit/FastCGI/HttpRequestTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of Swoole.
+ *
+ * @link     https://www.swoole.com
+ * @contact  team@swoole.com
+ * @license  https://github.com/swoole/library/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Swoole\FastCGI;
+
+use PHPUnit\Framework\TestCase;
+use Swoole\Coroutine;
+use Swoole\Coroutine\FastCGI\Client;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class HttpRequestTest extends TestCase
+{
+    /**
+     * To test the Keep-Alive header when sending multiple requests to a FastCGI server.
+     *
+     * @covers \Swoole\FastCGI\HttpRequest
+     * @see https://github.com/swoole/library/pull/169 Fix broken requests when keep-alive is turned on in the FastCGI client.
+     */
+    public function testKeepAlive(): void
+    {
+        Coroutine\run(
+            function (): void {
+                $client = new Client('php-fpm', 9000);
+
+                $request = (new HttpRequest())->withScriptFilename(DOCUMENT_ROOT . '/header0.php')->withKeepConn(false);
+                for ($i = 0; $i < 2; $i++) {
+                    self::assertSame(200, $client->execute($request)->getStatusCode(), 'Status code should always be 200 when HTTP header keep-alive is turned off.');
+                }
+
+                $request->withKeepConn(true);
+                for ($i = 0; $i < 2; $i++) {
+                    self::assertSame(200, $client->execute($request)->getStatusCode(), 'Status code should always be 200 when HTTP header keep-alive is turned on.');
+                }
+            }
+        );
+    }
+}
